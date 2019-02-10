ef2221e1e6c9e318a3d61fba0545f53ba969b01d
Aaron Gravelle <agrav12825@gmail.com>
2019-02-10 14:33:47 -0500

    Initial blank set up for project

---
+++ src/containers/Layout/Layout.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import style from './Layout.module.css';
+
+class Layout extends Component {
+    render() {
+        return (
+            <>
+            <main className={style.Content}>
+                 {this.props.children}
+            </main>
+            </>
+        )
+    }
+}
+
+export default Layout;
